97887fbdd48066f386273d507860d8712c753f36
Jiangqiu Shen <jshen2@mtu.edu>
2018-07-22 20:05:16 +0800

    add all

---
+++ findStringInPi/findStringInPi.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include<string>
+#define OUT_PUT
+// donwloadPi: https://pan.baidu.com/s/1qWZ9gWO
+bool findStringInPi(std::string input,unsigned long long & pos OUT_PUT);
---
+++ findStringInPi/main.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+int main(int argc, char const *argv[])
+{
+    std::cout<<"input the number you want to find in Pi :";
+    std::string Numbers;
+    std::cin>>Numbers;
+    std::cout<<std::endl<<"start to find the Number"<<std::endl;
+
+    int i=0;
+
+    return 0;
+}
